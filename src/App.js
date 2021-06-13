c1c0a9bd5a8fc5a3024f73963c08d5f179ba64b9
lacerta <kevinhickman4@gmail.com>
2021-06-12 20:01:23 -0500

    Added donation box and touched up functionallity

--- src/App.js
+++ src/App.js
@@ -51,7 +51,15 @@ useEffect(() => {
     <button onClick={handleClearItems}>Remove Selected Items From Shopping List</button>
     <ShoppingList items ={items} toggle={toggle} />
     <div>{items.filter(item => !item.complete).length} Item(s) on Shopping List</div>
-    <a href="https://www.taste.com.au/quick-easy/galleries/top-100-easy-dinner-recipes/biccuul7?page=2" target="_blank" class="nav-link">Click Here to Find Recipe Ideas!</a>
+    <a href="https://www.taste.com.au/quick-easy/galleries/top-100-easy-dinner-recipes/biccuul7?page=2" target="_blank" rel="noreferrer" class="nav-link">Click Here For Recipe Ideas!</a>
+    <form action="https://www.paypal.com/donate" method="post" target="_top">
+    <div>Donate to the Production of This App.</div>
+    <input type="hidden" name="business" value="NEDD2CEQV4EPN" />
+    <input type="hidden" name="amount" value="1" />
+    <input type="hidden" name="currency_code" value="USD" />
+    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+    </form>
     </>
   )
 }
